33b6f9af13caca4f3f633fb33a6ea0d7b46d74a0
char <char@aladinfun.com>
2017-07-14 17:19:08 +0800

    improve

--- code/Stress Testing/main.go
+++ code/Stress Testing/main.go
@@ -13,8 +13,8 @@ import (
 //服务器返回的信息
 type respMsg struct {
 	respbytes  int64
-	resptime   float64
 	serverInfo string
+	resptime   float64
 	doclen     int64
 }
 
@@ -23,7 +23,7 @@ func main() {
 	//命令行参数
 	cFlag := flag.Int("c", 1, "并发的连接数concurrent connects")
 	uFlag := flag.String("u", "http://localhost", "测试的URL")
-	mFlag := flag.String("m", "GEt", "http的请求方法")
+	mFlag := flag.String("m", "GEt", "http的请求方法, 暂时只有GET")
 
 	fmt.Println("method", *mFlag)
 
@@ -51,8 +51,8 @@ func main() {
 	fmt.Println("ServerInfo: ", respmsg.serverInfo)
 	fmt.Println("Server Host: ", strings.TrimPrefix(*uFlag, "http://"))
 	fmt.Println("Concurrent Level: ", *cFlag)
-	fmt.Printf("Total Time: %.2fs\n", totaltime) //请求时间
-	fmt.Println("timeslen: ", len(times))        //请求次数
+	fmt.Printf("Total Test Time: %.2fs\n", totaltime) //请求时间
+	fmt.Println("timeslen: ", len(times))             //请求次数
 	fmt.Println("Document length: ", respmsg.doclen)
 
 	//平均请求时间
@@ -60,7 +60,8 @@ func main() {
 	for _, i := range times {
 		avetime = avetime + i
 	}
-	fmt.Printf("averagetime: %.4fs\n", avetime/float64(len(times)))
+	fmt.Printf("Average Response Time: %.4fs\n", avetime/float64(len(times)))
+	fmt.Printf("Total Response Time: %.4fs\n", avetime)
 
 	//请求总字节数
 	var totalbytes int64
@@ -68,16 +69,15 @@ func main() {
 		totalbytes = totalbytes + i
 	}
 	fmt.Println("total transferred: ", totalbytes)
+	fmt.Println("Average transferred: ", totalbytes/int64(*cFlag))
 
 }
 
 // GoRequest ...
 // 发送请求
 func GoRequest(url string, ch chan respMsg) {
+
 	//var reqs *http.Request
-
-	start := time.Now()
-
 	// client := &http.Client{}
 	// b := strings.NewReader("name=cjb")
 
@@ -94,30 +94,26 @@ func GoRequest(url string, ch chan respMsg) {
 	// req.Header.Set()
 	// req.Header.Set()
 
+	start := time.Now()
 	respServer, err := http.Get(url)
+	resptime := time.Since(start).Seconds()
+
 	defer respServer.Body.Close()
 	if err != nil {
 		fmt.Println("response err: ", err)
 		return
 	}
+
 	respbytes, err := io.Copy(ioutil.Discard, respServer.Body)
 
 	serverInfo := respServer.Header.Get("Server")
-
 	doclen := respServer.ContentLength
 
-	resptime := time.Since(start).Seconds()
-	respmsg := respMsg{respbytes, resptime, serverInfo, doclen}
+	respmsg := respMsg{
+		respbytes,
+		serverInfo,
+		resptime,
+		doclen}
 
 	ch <- respmsg
 }
-
-// DealResp ...
-// 响应结果的处理
-// func DealResp(resp respMsg) {
-// 	respbytes, err := io.Copy(ioutil.Discard, resp.respServer.Body)
-// 	if err != nil {
-// 		fmt.Println("io.Copy err: ", err)
-// 	}
-// 	fmt.Println(respbytes)
-// }
